d08bc24aafd6f9c89705a010cbfcadbc97b17bdc
Taz Awad <tazawad@gmail.com>
2019-03-29 16:19:52 -0700

    Updating project

---
+++ Member_Demo/scripts/copy_files.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp /home/cn180291/Informatica/Informatica_Projects/Member_Demo/src/member_table1.csv /home/cn180291/Informatica/Informatica_Projects/Member_Demo/archived
+cp /home/cn180291/Informatica/Informatica_Projects/Member_Demo/src/member_table2.csv /home/cn180291/Informatica/Informatica_Projects/Member_Demo/archived
